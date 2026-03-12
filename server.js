32ccb5650e1259a7669572be20b4280f43f7e822
Santanu Saha <forever.santanu007@gmail.com>
2026-03-12 16:05:53 +0530

    fix render port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 app.use("/api", quizRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
